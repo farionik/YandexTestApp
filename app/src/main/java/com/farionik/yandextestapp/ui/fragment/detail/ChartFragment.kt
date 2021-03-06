29acd944f71930ff4829c8ffba907e522bcc48d4
viacheslav <farionik@gmail.com>
2021-03-06 17:20:26 +0300

    .

--- app/src/main/java/com/farionik/yandextestapp/ui/fragment/detail/ChartFragment.kt
+++ app/src/main/java/com/farionik/yandextestapp/ui/fragment/detail/ChartFragment.kt
@@ -1,16 +1,21 @@
 package com.farionik.yandextestapp.ui.fragment.detail
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.farionik.yandextestapp.R
 import com.farionik.yandextestapp.databinding.FragmentChartBinding
 import com.farionik.yandextestapp.ui.fragment.BaseFragment
 import com.farionik.yandextestapp.ui.util.formatChangeValue
 import com.farionik.yandextestapp.ui.util.formatPercentValue
 import com.farionik.yandextestapp.ui.util.formatPriceValue
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IFillFormatter
 
 class ChartFragment : BaseFragment() {
 
@@ -27,6 +32,8 @@ class ChartFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initChart()
+
         companyDetailViewModel.companyDetailModelLiveData.observe(viewLifecycleOwner, {
             with(binding) {
                 lastPrice.formatPriceValue(it)
@@ -37,5 +44,51 @@ class ChartFragment : BaseFragment() {
                 btnBuy.text = buyButtonText
             }
         })
+    }
+
+    private fun initChart() {
+
+        val entries = mutableListOf<Entry>()
+        for (i in 0..40) {
+            entries.add(Entry(i.toFloat(), Math.random().toFloat()))
+        }
+        val lineDataSet = LineDataSet(entries, "Data set").apply {
+            mode = LineDataSet.Mode.CUBIC_BEZIER
+            cubicIntensity = 0.2f
+            setDrawFilled(true)
+            setDrawCircles(false)
+            lineWidth = 2f
+            circleRadius = 4f
+            setCircleColor(ContextCompat.getColor(binding.chart.context, R.color.black))
+            //highLightColor = Color.rgb(244, 117, 117)
+            color = ContextCompat.getColor(binding.chart.context, R.color.black)
+            fillColor = Color.WHITE
+            fillAlpha = 100
+            setDrawHorizontalHighlightIndicator(false)
+            /*set1.setFillFormatter(IFillFormatter { dataSet, dataProvider ->
+                chart.getAxisLeft().getAxisMinimum()
+            })*/
+        }
+        val data = LineData(lineDataSet)
+//        data.setValueTypeface(tfLight)
+        data.setValueTextSize(9f)
+        data.setDrawValues(false)
+
+        binding.chart.run {
+            setViewPortOffsets(0f, 0f, 0f, 0f)
+            setBackgroundColor(Color.WHITE)
+            description.isEnabled = false
+            setTouchEnabled(false)
+            isDragEnabled = false
+            setScaleEnabled(false)
+            setPinchZoom(false)
+            setDrawGridBackground(false)
+            legend.isEnabled = false
+            //setBackgroundColor(Color.rgb())
+            setData(data)
+            invalidate()
+        }
+
+
     }
 }
